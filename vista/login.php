39e556113897bbc858cb46ce0ec726fad6b5b409
AlidesDaza <adaza@hotmail.com>
2024-03-10 21:46:44 -0300

    first commit

---
+++ vista/login.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../css/style.css"> 
+    <link rel="stylesheet" type="text/css" href="../css/css/all.min.css"> 
+</head>
+<body>
+    <img class="wave" src="../img/wave.png" alt="">
+    <div class="contenedor">
+        <div class="img">
+            <img src="../img/bg.svg" alt="">
+        </div>
+        <div class="contenindo-login">
+            <form action="">
+                <img src="../img/logo.png" alt="">
+                <h2>Farmacia</h2>
+                <div class="input-div dni">
+                    <div class="i">
+                        <i class="fas fa-user"></i>
+                    </div>
+                    <div class="div">
+                        <h5>DNI</h5>
+                        <input type="text" name="user" class="input">
+                    </div>
+                </div>
+                <div class="input-div pass">
+                <div class="i">
+                        <i class="fas fa-lock"></i>
+                    </div>
+                    <div class="div">
+                        <h5>contrasena</h5>
+                        <input type="password" name="pass" class="input">
+                    </div>
+                </div>
+                <a href="">Create warplace</a>
+                <input type="submit" class="btn" values="Iniciar Sesion">
+            </form>
+
+        </div>
+    </div>
+</body>
+</html>
